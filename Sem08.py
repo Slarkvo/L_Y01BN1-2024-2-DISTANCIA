f9bfbeebfe7ec869cb60441add43384d384b4e8c
Slarkvo <slarkvo@gmail.com>
2024-10-28 21:52:52 -0500

    profe

--- Sem08.py
+++ Sem08.py
@@ -1,38 +1,38 @@
-import streamlit as streamlit
+import streamlit as st
 
 def validate_data(marca, modelo, kilometraje):
-    """Valida los datos ingresados para el automovil."""
+    """Valida los datos ingresados para el automóvil."""
     if not marca or not modelo:
-        return "La marca y el modelo no deben estar vacìos"
+        return "La marca y el modelo no deben estar vacíos."
     try:
         kilometraje = float(kilometraje)
         if kilometraje < 0:
             return "El kilometraje no puede ser menor que 0."
-    except Valuerror:
-        return "El kilometraje debe ser un nùmero vàlido."
+    except ValueError:
+        return "El kilometraje debe ser un número válido."
     return None
 
 def main():
-    st.title("Registro de automòvil")
-    st.write("Ingrese los datos del automòvil a continuaciòn")
-    
+    st.title("Registro de Automóvil")
+    st.write("Ingrese los datos del automóvil a continuación:")
+
     #Registro por el usuario
-    marca = st.text_input("Marca del automòvil")
-    modelo = st.text_input("Modelo del automòvil")
-    kilometraje = st.text_input("Kilometraje del automòvil")
+    marca = st.text_input("Marca del automóvil")
+    modelo = st.text_input("Modelo del automóvil")
+    kilometraje = st.text_input("Kilometraje del automóvil")
 
     if st.button("Registrar"):
-        #Validacion de los datos
+        #validación de los datos
         error = validate_data(marca, modelo, kilometraje)
         if error:
             st.error(error)
         else:
-            st.success("Automòvil registrado exitosamente.")
+            st.success("Automóvil registrado exitosamente.")
             st.write("**Marca:**",marca)
             st.write("**Modelo:**",modelo)
-            st.write("**Kilpometraje:**",kilometraje)
-    
+            st.write("**Kilometraje:**", kilometraje)
+
 if __name__ == "__main__":
-        main()
+    main()
 
 
